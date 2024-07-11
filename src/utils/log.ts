fd4ba000516828f579358eb5ae9c69043e1e7b1f
Louie <louie.spena@outlook.com>
2024-07-11 20:58:16 -0300

    ADD: date to consoleLog

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -3,6 +3,8 @@ import { gray, blue, red } from 'chalk';
 const nowString = () =>
 	new Date().toLocaleTimeString('pt-BR', {
 		timeZone: 'America/Sao_Paulo',
+		day: '2-digit',
+		month: '2-digit',
 		hour: '2-digit',
 		minute: '2-digit',
 		second: '2-digit',
