c300f2f1aa71c4e0aac606c936d45bf4fdc59650
Louie <louie.spena@outlook.com>
2024-09-10 11:49:25 -0300

    CI/CD: npm production script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
 	apps: [
 		{
 			name: 'coletivo-bot',
-			script: 'dist/index.js',
+			script: 'npm',
+			args: ['run', 'production'],
 			env: {
 				NODE_ENV: 'prod',
 				FORCE_COLOR: 1,
